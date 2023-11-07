a96e847bdf1af6b92ff53e550c4d3b92a13aff3f
coolmarvel <marvel97@naver.com>
2023-11-07 18:06:43 +0900

    ledger library update

--- test.js
+++ test.js
@@ -5,29 +5,41 @@ const getProvider = require("./utils/provider");
 
 const signTransactionByLedger = async () => {
   try {
-    // const transport = await Transport.create();
-    // const eth = await Eth(transport);
+    const transport = await Transport.create();
+    const eth = new Eth(transport);
 
     const web3 = await getProvider();
 
-    // const hdPath = "m/44'/60'/0'/0/0";
-    // const result = await eth.getAddress(hdPath);
-    // console.log(`Ledger address: ${result.address}`);
+    const hdPath = "m/44'/60'/0'/0/0";
+    const address = (await eth.getAddress(hdPath)).address;
+    console.log(`Ledger address: ${address}`);
 
-    const txObject = {};
-    txObject.from = "5dc93ef0344a9d86fe6e172566a744e8e3078bf8";
-    // txObject.from = result.address;
-    txObject.to = "eafcd375e7868a351c1f24644cd435461dbf5945";
-    txObject.value = web3.utils.toWei("0.1", "ether");
-    txObject.gas = "21000";
-    txObject.gasPrice = await web3.eth.getGasPrice();
-    txObject.nonce = await web3.eth.getTransactionCount(txObject.from);
-    txObject.chainId = web3.utils.toHex(await web3.eth.getChainId());
+    // console.log(eth);
 
-    const signedTx = await web3.eth.signTransaction(txObject);
+    const tx = {};
+    tx.from = address;
+    tx.to = "0x5dc93ef0344a9d86fe6e172566a744e8e3078bf8";
+    tx.value = web3.utils.toWei("0.1", "ether");
+    tx.gas = "21000";
+    tx.gasPrice = await web3.eth.getGasPrice();
+    tx.nonce = await web3.eth.getTransactionCount(tx.from);
+    tx.chainId = await web3.eth.getChainId();
+
+    console.log(tx);
+
+    const {ledgerService} = require("@ledgerhq/hw-app-eth");
+    const Utils = require("ethereumjs-util");
+    const RLP = Utils.rlp;
+
+    const txRLP = RLP.encode([tx.chainId, tx.nonce, tx.from, tx.to, tx.value, tx.gas, tx.gasPrice]);
+    const typeTxHex = "0x02" + Utils.bufferToHex(txRLP).slice(2);
+    const txHash = Utils.bufferToHex(Utils.toBuffer(typeTxHex));
+
+    const resolution = await ledgerService.resolveTransaction(txHash);
+    console.log(resolution);
+
+    const signedTx = await eth.signTransaction(hdPath, txHash, resolution);
     console.log(signedTx);
-
-    // const signedTx = await eth.signTransaction(hdPath);
   } catch (error) {
     console.error(error.message);
   }
